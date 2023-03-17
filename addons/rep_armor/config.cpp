bb8d100b3d3e1c828ea14cf697efd98e3b8f74ff
Nacix <anorexicanalt@gmail.com>
2023-03-16 20:30:56 -0400

    feat!: add rep_armor module with new assets
    Added rep_armor module to replace armor module
    
    Added Alderaanian armor set
    
    Updated Havoc armor set
    
    Fixed armor LOD not appearing in first person view
    
    Refactored classnames for better future-proofing

---
+++ addons/rep_armor/config.cpp
@@ -0,0 +1,38 @@
+#include "script_component.hpp"
+
+class CfgPatches
+{
+    class ADDON
+    {
+        name = COMPONENT_NAME;
+		requiredVersion = REQUIRED_VERSION;
+        author = ECSTRING(common,TORTeam);
+		authors[] = {"Moose, Stim, Anorexican"};
+		url = ECSTRING(main,URL);
+        requiredAddons[]=
+        {
+            "cba_main",
+            "A3_data_F",
+            "A3_anims_F",
+			"A3_weapons_F",
+			"A3_characters_F"
+        };
+        weapons[]=
+        {
+            "TOR_RT_Helmet",
+            "TOR_RT_Helmet_Alderaan",
+            "TOR_RT_Helmet_Alderaan_NCO",
+            "TOR_RT_Helmet_Havoc",
+            "TOR_RT_Helmet_Havoc_NCO",
+            "TOR_RT_Visor",
+            "TOR_RT_Visor_Alderaan",
+            "TOR_RT_Visor_Havoc",
+            "TOR_RT_Vest",
+            "TOR_RT_Vest_Alderaan",
+            "TOR_RT_Vest_Havoc"
+        };
+        units[] = {};
+    };
+};
+
+#include "CfgWeapons.hpp"
---
+++ addons/rep_armor/script_component.hpp
@@ -0,0 +1,6 @@
+#define COMPONENT rep_armor
+#define COMPONENT_BEAUTIFIED Republic Armor
+
+#include "\x\tor\addons\main\script_mod.hpp"
+
+#include "\x\tor\addons\main\script_macros.hpp"
